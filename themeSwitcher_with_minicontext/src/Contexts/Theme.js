717d7c6fbb9e38d7fd1a316f7f99eb7514c6183f
developerfozan <shaikhfozan192@gmail.com>
2025-06-12 11:17:05 +0530

    Dark And Light Theme With Toggle

---
+++ themeSwitcher_with_minicontext/src/Contexts/Theme.js
@@ -0,0 +1,19 @@
+import { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
+
+// import { createContext } from "react";
+
+// export const ThemeContext = createContext(); // export the context
+
+// export const ThemeProvider = ThemeContext.Provider; // export the provider
